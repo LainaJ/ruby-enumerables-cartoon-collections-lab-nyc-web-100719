30cebc391a2845a57e19e6c23e7ecbf8fdd76e2d
Laina Karosic <lainakarosic@gmail.com>
2019-09-14 18:25:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,15 +8,14 @@ end
 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element| 
+  planeteer_calls.map do |element|
   element.capitalize + "!"
   end
 end
 
 def long_planeteer_calls(calls)
   #count number of characters
-
-  calls.any? do |calls| 
+  calls.any? do |calls|
   calls.length > 4
   end
 end
