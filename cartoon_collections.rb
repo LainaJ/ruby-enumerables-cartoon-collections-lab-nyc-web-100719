7c849a0725291916b14f81221603558fc5c706a2
Laina Karosic <lainakarosic@gmail.com>
2019-09-14 18:09:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  
+  #count number of characters 
+  planeteer_calls[i].length > 4 ? true:false
+  #condition to return if false
 end
 
 def find_the_cheese# code an argument here
