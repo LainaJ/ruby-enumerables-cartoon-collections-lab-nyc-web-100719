72362ebdb5ba3cbff42ab422c89fffacc51746db
Laina Karosic <lainakarosic@gmail.com>
2019-09-14 18:51:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  array.detect do |variety|
+  array.find do |variety|
     array.include? (variety)
   end
 end
